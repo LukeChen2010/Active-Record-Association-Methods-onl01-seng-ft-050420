69cf9a787ad71d737c384b3fe8457c2c9a6d7d22
Luke Chen <luke.chen.2010@gmail.com>
2020-05-27 14:43:39 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,16 +4,16 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
-    return self.songs.count
+    return @songs.count
   end
 
   def artist_count
     # return the number of artists associated with the genre
-    return self.artists.count
+    return @artists.count
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    return artists.name
+    return 
   end
 end
