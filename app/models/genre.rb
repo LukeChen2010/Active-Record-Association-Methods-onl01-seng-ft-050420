9ef79d8a3a405a2851853bed50aa8cdef304035a
Luke Chen <luke.chen.2010@gmail.com>
2020-05-27 14:44:50 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,6 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    return artists,names
+    return artists.name
   end
 end
